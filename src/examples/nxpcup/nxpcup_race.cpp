fe7f4a2318cc93d3d50e97bbc75055566b662c97
SaAPro <alexandre_sa@live.fr>
2020-04-27 11:48:21 +0200

    develop algorithm to generate speed and steer command from pixy's vector output

--- src/examples/nxpcup/nxpcup_race.cpp
+++ src/examples/nxpcup/nxpcup_race.cpp
@@ -44,12 +44,68 @@
 #include <string.h>
 #include <math.h>
 
+int x_origin = 39;
+int y_origin = 52;
+int count = 0;
+int count_stop = 100;
+
+int8_t res;
+
+roverControl control_saved;
 
 roverControl raceTrack(Pixy2 &pixy)
 {
 	roverControl control{};
-	/* instert you algorithm here */
 
+	float delta, norme;
+
+	res = pixy.line.getMainFeatures();
+
+	pixy.line.vectors->print();
+
+	// PX4_INFO("Pixy:\t%u vectors found", pixy.line.numVectors);
+
+	// for (int i = 0; i <= pixy.line.numVectors; i++) {
+	// 	pixy.line.vectors[i].print();
+	// }
+
+	// pixy.line.vectors[0].m_x0;
+	// pixy.line.vectors[0].m_x1;
+	// pixy.line.vectors[0].m_y0;
+	// pixy.line.vectors[0].m_y1;
+
+	// Control information:
+	// speed: -1 (back) to +1 (front)
+	// speed: (m/s)
+	// steering: -1 (left) to +1 (right)
+	// steering: rad -pi (left) to +pi (right)
+
+	if (res & LINE_VECTOR) {
+		norme = sqrt(pow(pixy.line.vectors->m_x1 - x_origin, 2) + pow(pixy.line.vectors->m_y1 - y_origin, 2));
+
+		delta = acos(abs(pixy.line.vectors->m_y1 - y_origin) / norme);
+		delta = copysign(delta, pixy.line.vectors->m_x1 - x_origin);
+
+		control.speed = 0.25 * cos(0.5F * delta);
+		control.steer = delta;
+
+		control_saved = control;
+
+		if (count != 0) { count = 0; }
+
+		PX4_INFO("speed: %f [m/s]", (double)control.speed);
+		PX4_INFO("delta: %f [deg]", (double)delta * 180.0 / M_PI);
+
+	} else {
+		if (count < count_stop) {
+			control = control_saved;
+			count++;
+
+		} else {
+			control.speed = 0.0;
+			control.steer = 0.0;
+		}
+	}
 
 	return control;
 }
